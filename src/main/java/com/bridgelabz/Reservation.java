c0e762d75df301e7ac93770b8a5d2826fedd15ed
vijikumar123 <vk9164940756@gmail.com>
2023-02-13 21:30:01 +0530

    Added: uc1 problem

---
+++ src/main/java/com/bridgelabz/Reservation.java
@@ -0,0 +1,41 @@
+package com.bridgelabz;
+
+public class Reservation {
+    public Reservation() {
+    }
+    public Reservation(integer id) {
+        this.id = id;
+        this.idRoom = idRoom;
+    }
+    public integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public int getIdRoom() {
+        return idRoom;
+    }
+    public void setIdRoom(int idRoom) {
+        this.idRoom = idRoom;
+    }
+    public Date getCheckintDate() {
+        return checkinDate;
+    }
+    public void setCheckInDate(Date checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+
+
+    }
+}
